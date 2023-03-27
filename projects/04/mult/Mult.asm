c3067083d0b6d5880308e378784232373cd8bd9a
neelg07 <ineel7762@gmail.com>
2023-03-27 18:36:07 -0400

    Complete Mult program implementation

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,41 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+    @R0         // initialize variables
+    D=M
+    @x
+    M=D         // x = R0 
+
+    @R1
+    D=M
+    @y
+    M=D         // y = R1
+
+    @i
+    M=0         // i = 0 (iteration variable of loop)
+
+    @R2
+    M=0         // product = 0
+
+(LOOP)
+    @y          // Check if loop should end or not
+    D=M         // D = y
+    @i
+    D=D-M       // D = y - i
+    @END
+    D;JEQ       // If (i == y) goto END
+            
+    @x
+    D=M            // Add x to product each iteration
+    @R2
+    M=D+M
+
+    @i              // update i variable each iteration
+    M=M+1
+    @LOOP
+    0;JMP           // Goto loop
+
+(END)
+    @END
+    0;JMP       // Infinite loop
